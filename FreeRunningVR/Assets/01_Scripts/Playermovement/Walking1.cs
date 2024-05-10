41b2efb6317813e4e845b47a997544d101c840bb
Blade412wild <nathanhoh951@gmail.com>
2024-05-10 14:49:31 +0200

    Jumping Based on Hands Works

--- FreeRunningVR/Assets/01_Scripts/Playermovement/Walking1.cs
+++ FreeRunningVR/Assets/01_Scripts/Playermovement/Walking1.cs
@@ -11,7 +11,9 @@ public class Walking1 : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private float groundDrag;
 
-    [SerializeField] private float jumnpForce;
+    [SerializeField] private float smallJumnpForce;
+    [SerializeField] private float bigJumnpForce;
+
     [SerializeField] private float jumpCooldown;
     [SerializeField] private float airMultiplier;
     private bool readyToJump = true;
@@ -20,15 +22,27 @@ public class Walking1 : MonoBehaviour
     [SerializeField] private KeyCode jumpKey = KeyCode.Space;
     [SerializeField] private InputActionProperty JumpInputSource;
 
-
-
     [Header("GroundCheck")]
     [SerializeField] private float playerHeight;
     [SerializeField] private LayerMask whatIsGround;
+
+    [Header("Jump")]
+    [SerializeField] private float JumpVelocityMin = 2.0f;
+    [SerializeField] private float JumpTravelDistanceMin = 0.2f;
     public bool grounded;
 
-
+    [Header("GameObjects")]
+    [SerializeField] private Rigidbody leftHandRB;
+    [SerializeField] private Rigidbody rightHandRB;
+    [SerializeField] private Transform leftHandTransform;
+    [SerializeField] private Transform rightHandTransform;
     [SerializeField] private Transform orientation;
+
+    private List<float> leftHandRecordings = new List<float>();
+    private List<float> rightHandRecordings = new List<float>();
+
+    private Vector3 leftHandInitialPos;
+    private Vector3 rightHandInitialPos;
     private float horizontalInput;
     private float verticalInput;
 
@@ -41,7 +55,9 @@ public class Walking1 : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         InputManager.Instance.playerInputActions.Walking.Enable();
-        InputManager.Instance.playerInputActions.Walking.TryToJump.canceled += TryToJump;
+        InputManager.Instance.playerInputActions.Walking.TryToJump.canceled += DicideHowToJump;
+        InputManager.Instance.playerInputActions.Walking.TryToJump.started += SetJumpData;
+
     }
 
     // Update is called once per frame
@@ -77,7 +93,7 @@ public class Walking1 : MonoBehaviour
         if (Input.GetKey(jumpKey) && readyToJump && grounded)
         {
             readyToJump = false;
-            Jump();
+            SmallJump();
 
             Invoke(nameof(resetJump), jumpCooldown);
         }
@@ -115,12 +131,21 @@ public class Walking1 : MonoBehaviour
         }
     }
 
-    private void Jump()
+    private void SmallJump()
     {
+        Debug.Log("SmallJump");
         // reset y Velocity
         rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
 
-        rb.AddForce(transform.up * jumnpForce, ForceMode.Impulse);
+        rb.AddForce(transform.up * smallJumnpForce, ForceMode.Impulse);
+    }
+
+    private void BigJump()
+    {
+        Debug.Log("biggJump");
+        rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+
+        rb.AddForce(transform.up * bigJumnpForce, ForceMode.Impulse);
     }
 
     private void resetJump()
@@ -128,18 +153,63 @@ public class Walking1 : MonoBehaviour
         readyToJump = true;
     }
 
-    private void TryToJump(InputAction.CallbackContext context)
+    private void SetJumpData(InputAction.CallbackContext context)
     {
-        //rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
-        float input = context.ReadValue<float>();
-        Debug.Log(input);
-        if (readyToJump && grounded)
-        {
-            readyToJump = false;
-            Jump();
+        leftHandInitialPos = leftHandTransform.position;
+        rightHandInitialPos = rightHandTransform.position;
+    }
 
+
+    private void DicideHowToJump(InputAction.CallbackContext context)
+    {
+        if (!readyToJump && !grounded) return;
+
+        Vector3 leftHandEndPos = leftHandTransform.position;
+        Vector3 rightHandEndPos = rightHandTransform.position;
+
+        if (leftHandEndPos.y < leftHandInitialPos.y || rightHandEndPos.y < rightHandInitialPos.y)
+        {
+            SmallJump();
+            readyToJump = false;
             Invoke(nameof(resetJump), jumpCooldown);
         }
+
+        float leftHandDistanceTraveled = leftHandEndPos.y - leftHandInitialPos.y;
+        float rightHandDistaneTraveled = rightHandEndPos.y - rightHandInitialPos.y;
+
+        //leftHandRecordings.Add(leftHandDistanceTraveled);
+        //rightHandRecordings.Add(rightHandDistaneTraveled);
+        //Debug.Log("lefthand Avarage : " + calculateAvarage(leftHandRecordings));
+        //Debug.Log("righthand Avarage : " + calculateAvarage(rightHandRecordings));
+
+        if (leftHandDistanceTraveled < 0.2f && rightHandDistaneTraveled < 0.2f) return;
+
+        Vector3 leftHandEndVel = leftHandRB.velocity;
+        Vector3 rightHandEndVel = rightHandRB.velocity;
+
+        if (leftHandEndVel.y < 2.0f && rightHandEndVel.y < 3.0f) return;
+
+        //leftHandRecordings.Add(lefthandEndVel.y);
+        //rightHandRecordings.Add(rightHandEndVel.y);
+        //Debug.Log("lefthand Avarage : " + calculateAvarage(leftHandRecordings));
+        //Debug.Log("righthand Avarage : " + calculateAvarage(rightHandRecordings));
+
+        BigJump();
+        readyToJump = false;
+        Invoke(nameof(resetJump), jumpCooldown);
+    }
+
+    private float calculateAvarage(List<float> list)
+    {
+        float allNumbers = 0;
+
+        foreach (var number in list)
+        {
+            allNumbers += number;
+        }
+
+        float average = allNumbers / list.Count;
+        return average;
     }
 
     Vector2 ReadJoystickInput(InputAction.CallbackContext context)
