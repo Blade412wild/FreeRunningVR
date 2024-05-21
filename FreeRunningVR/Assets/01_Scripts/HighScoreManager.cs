5c979dcfde5c3573f079a47edbf3bf4e42b9db52
Blade412wild <nathanhoh951@gmail.com>
2024-05-21 12:40:49 +0200

    List Is Saveable

--- FreeRunningVR/Assets/01_Scripts/HighScoreManager.cs
+++ FreeRunningVR/Assets/01_Scripts/HighScoreManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -7,13 +8,24 @@ using UnityEngine;
 public class HighScoreManager : MonoBehaviour
 {
     public LevelManager levelManager;
+    public bool save = false;
     [SerializeField] private string Name;
+    [SerializeField] private string fileName;
+    [SerializeField] private float maxHighScores;
     private string fullPath;
-    private string fileName;
 
     private void Start()
     {
         levelManager.OnEndLevel += TryToSavePlayerData;
+    }
+
+    private void Update()
+    {
+        if (save)
+        {
+            SaveList();
+            save = false;
+        }
     }
 
 
@@ -76,20 +88,116 @@ public class HighScoreManager : MonoBehaviour
 
     }
 
+    private void SaveList()
+    {
+        PlayerDataStruct hoi = new PlayerDataStruct();
+        hoi._name = "Nathan";
+        hoi._time = 18.0f;
+        hoi._rank = 1;
+
+        PlayerDataStruct hoi2 = new PlayerDataStruct();
+        hoi2._name = "Felice";
+        hoi2._time = 38.0f;
+        hoi2._rank = 2;
+
+        PlayerDataStruct hoi3 = new PlayerDataStruct();
+        hoi2._name = "Simon";
+        hoi2._time = 40.0f;
+        hoi2._rank = 3;
+
+        LevelDataStruct levelDataStruct = new LevelDataStruct();
+        levelDataStruct._level = 1;
+        levelDataStruct._highScores = new List<PlayerDataStruct>
+        {
+            hoi,
+            hoi2,
+            hoi
+        };
+
+        string path = GetPath();
+        StreamWriter streamWriter = new StreamWriter(path, false);
+        streamWriter.WriteLine(JsonUtility.ToJson(levelDataStruct, true));
+        streamWriter.Close();
+        streamWriter.Dispose();
+
+        LoadList(path);
+    }
+
+    private void LoadList(string path)
+    {
+        if (File.Exists(path) == false) return;
+        StreamReader reader = new StreamReader(path);
+        LevelDataStruct levelData = JsonUtility.FromJson<LevelDataStruct>(reader.ReadToEnd());
+        reader.Close();
+        reader.Dispose();
+        Debug.Log(levelData);
+    }
+
+    private void CheckHighScoreBoard(List<PlayerDataStruct> oldHighscoreData, PlayerDataStruct currentPlayerData)
+    {
+        // seperate the score;
+        List<float> scores = new List<float>();
+        foreach (PlayerDataStruct PlayerDataStruct in oldHighscoreData)
+        {
+            scores.Add(PlayerDataStruct._time);
+        }
+
+        scores.Sort();
+        bool scoreIsGoodEnough = false;
+
+        // create new HighScore
+        List<float> newHighScoreOrder = new List<float>();
+
+        foreach (float score in scores)
+        {
+            if (currentPlayerData._time > score && scoreIsGoodEnough == false)
+            {
+                newHighScoreOrder.Add(currentPlayerData._time);
+                scoreIsGoodEnough = true;
+            }
+            else
+            {
+                newHighScoreOrder.Add(score);
+            }
+        }
+
+        newHighScoreOrder.Sort();
+
+        // update de groote van de highscores
+        if (newHighScoreOrder.Count >= maxHighScores && scoreIsGoodEnough)
+        {
+            newHighScoreOrder.Remove(newHighScoreOrder[newHighScoreOrder.Count - 1]);
+        }
+
+        CreateNewHighScore(oldHighscoreData, newHighScoreOrder);
+    }
+
+    private void CreateNewHighScore(List<PlayerDataStruct> playerDataStructs, List<float> scores)
+    {
+
+        foreach(float score in scores)
+        {
+
+        }
+    }
+
+
+
 
 }
 
-struct PlayerDataStruct
+[Serializable]
+public struct PlayerDataStruct
 {
     public string _name;
     public float _time;
     public int _rank;
 }
 
-struct LevelDataStruct
+[Serializable]
+public struct LevelDataStruct
 {
     public int _level;
     public List<PlayerDataStruct> _highScores;
-
-
 }
+
